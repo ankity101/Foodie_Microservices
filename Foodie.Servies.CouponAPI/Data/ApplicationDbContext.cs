6a8046335b0561abec9c9f527f485a7941f2406c
ankity101 <ankit.y0722@gmail.com>
2023-08-24 17:36:36 +0530

    Add project files.

---
+++ Foodie.Servies.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Foodie.Servies.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foodie.Servies.CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+                
+        }
+
+        public DbSet<Coupon> Coupons    { get; set; }
+
+    }
+}
---
+++ Foodie.Servies.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Foodie.Servies.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
