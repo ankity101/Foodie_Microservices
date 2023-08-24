79305b88bf20512bb6c1cda421d15f4ae1cc97b2
ankity101 <ankit.y0722@gmail.com>
2023-08-24 17:51:08 +0530

    Auto Migration Method Added

--- Foodie.Servies.CouponAPI/Data/ApplicationDbContext.cs
+++ Foodie.Servies.CouponAPI/Data/ApplicationDbContext.cs
@@ -12,5 +12,26 @@ namespace Foodie.Servies.CouponAPI.Data
 
         public DbSet<Coupon> Coupons    { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+               new Coupon
+               {
+                   CouponId = 1,
+                   CouponCode = "DIS100",
+                   DiscountAmount = 100,
+                   MinAmount = 1000
+               },
+               new Coupon
+               {
+                   CouponId = 2,
+                   CouponCode = "DIS50",
+                   DiscountAmount =50,
+                   MinAmount = 500
+               }
+               ) ;
+        }
     }
 }
---
+++ Foodie.Servies.CouponAPI/Migrations/20230824121455_SeedingDataInCouponTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Foodie.Servies.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingDataInCouponTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "DIS100", 100.0, 1000 },
+                    { 2, "DIS50", 50.0, 500 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+        }
+    }
+}
